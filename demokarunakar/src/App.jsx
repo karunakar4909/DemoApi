9f7d94bf87f3c1b080f1233522c14f3f0c36d6a3
karunakar <karunakara7229203@gmail.com>
2023-06-25 19:32:50 +0530

    Changed something else

---
+++ demokarunakar/src/App.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import "./index.css";
+
+function App() {
+  const [users, setUsers] = React.useState([]);
+  const f = async () => {
+   const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    setUsers(json.data);
+    console.log(res);
+  };
+  React.useEffect(() => {
+    f();
+  }, []);
+  return (
+    <div className="App">
+    <h1>Hello ReqRes users!</h1>
+    <div className="flex">
+      {users.length &&
+        users.map((user) => {
+          return (
+            <div key={user.id}>
+              <p>
+                <strong>{user.first_name}</strong>
+              </p>
+              <p>{user.email}</p>
+              <img key={user.avatar} src={user.avatar} />
+            </div>
+          );
+        })}
+    </div>
+  </div>
+  );
+}
+export default App;
